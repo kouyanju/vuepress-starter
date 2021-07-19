ac3403729c4af657358f8fefdc8a95f08c77ccba
kouyanju <17865815392@163.com>
2021-07-19 15:32:20 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,16 +9,35 @@ module.exports = {
       ['meta', { name: 'author', content: '吃饭睡觉奋斗斗' }],
       ['meta', { name: 'keywords', content: '吃饭睡觉奋斗斗的笔记介绍' }]
     ],
-    // plugins: [
-    //   [
-    //     '@vuepress/last-updated',
-    //     {
-    //       transformer: (timestamp) => {
-    //         return moment(timestamp).fromNow()
-    //       }
-    //     }
-    //   ]
-    // ],
+    plugins: {
+       // 
+      //   '@vuepress/last-updated',
+      //   {
+      //     transformer: (timestamp) => {
+      //       return moment(timestamp).fromNow()
+      //     }
+      //   }
+      // ],
+      '@vssue/vuepress-plugin-vssue': {
+        // set `platform` rather than `api`
+        platform: 'github-v4',
+  
+        // all other options of Vssue are allowed
+        owner: 'kouyanju',
+        repo: 'vuepress-starter',
+        clientId: '4aa8eeefc0ec4bc07b8d',
+        clientSecret: 'a707849af241ad6faebb3be0831c8024738a6e72',
+      },
+      '@vuepress/pwa':{
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新"
+        }
+      }
+
+    },
+     
     themeConfig: {
       lastUpdated: '更新时间',
       logo: '/assets/img/hero.png',
